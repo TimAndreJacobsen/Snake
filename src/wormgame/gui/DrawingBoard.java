769712580d73887d082484843928a3dea84280f6
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-12 23:21:33 +0100

    initial commit - picked up project again

--- src/wormgame/gui/DrawingBoard.java
+++ src/wormgame/gui/DrawingBoard.java
@@ -2,7 +2,6 @@ package wormgame.gui;
 
 import wormgame.domain.Piece;
 import wormgame.game.WormGame;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,17 +12,21 @@ public class DrawingBoard extends JPanel implements Updatable {
 
     public DrawingBoard(WormGame wormGame, int pieceLength) {
         this.wormGame = wormGame;
-        this.pieceLength = pieceLength; // Size of either height / length
+        this.pieceLength = pieceLength;
     }
 
     @Override
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-
+        // Background
+        graphics.setColor(Color.GRAY);
+        graphics.fillRect(0,0,getWidth(),getHeight());
+        // Worm
         for (Piece p : wormGame.getWorm().getPieces() ) {
             graphics.setColor(Color.BLACK);
             graphics.fill3DRect(p.getX() * pieceLength,p.getY() * pieceLength,pieceLength,pieceLength,true);
         }
+        //Apples
         graphics.setColor(Color.RED);
         graphics.fillOval(wormGame.getApple().getX() * pieceLength, wormGame.getApple().getY() * pieceLength, pieceLength, pieceLength);
     }
